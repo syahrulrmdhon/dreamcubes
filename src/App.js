5dd7c49d227caa6517cb4a9ae17f63e3d55ea9e9
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-21 19:24:36 +0700

    add main card and run.png assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import laptop from './laptop.png';
 import phone from './phone.png';
+import run from './run.png';
 import './App.css';
 
 class App extends Component {
@@ -28,6 +29,27 @@ class App extends Component {
               <div className="card-content">
                 <p>Step 2</p>
                 <h3>Lorem ipsum dolor sit amet, persecuti dissentiet cum cu. </h3></div>
+            </div>
+          </article>
+          <article className="articleShort articleMain">
+            <div className="card">
+              <div className="card-image" style={{backgroundColor:'linen'}}>
+                  <img src={run} alt="alt here"/>
+              </div>
+              <div className="card-content">
+                <p>Tentang AWR 2018</p>
+                <h3>Allianz World Run 2018</h3>
+                <p>Lorem ipsum dolor sit amet, persecuti dissentiet cum cu. 
+                  Mel esse meliore detracto ei, qui quem magna te. An liber audiam ocurreret eum, 
+                  vim eu fastidii periculis. </p>
+                <p><a href=''>Show less detail</a></p>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet</li>
+                  <li>persecuti dissentiet cum cu</li>
+                  <li>Mel esse meliore detracto ei,</li>
+                  <li>An liber audiam ocurreret eum,</li>
+                </ul>
+              </div>
             </div>
           </article>
           </div>
